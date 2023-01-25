69f74fd1cb6d32ceea49c2779bf97b3c3298ff7e
Ahmed <ahmed_hany19@outlook.com>
2023-01-25 15:27:42 +0200

    Setup Context

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,12 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App";
+import { AuthProvider } from "./Context/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
